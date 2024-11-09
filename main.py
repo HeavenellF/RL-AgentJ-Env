b35560e23ecd6ef7a325e99c801ff4a362eaec8d
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-11-09 21:52:19 +0100

    remove some unnecessary line
    no more jump power

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Agent J')    # Window Name here
 clock = pygame.time.Clock()
 
-game_state = 1
-
 #========================== LEVELS OBJECT ================================#
 levels_object = [levelsobject.level1_object(width,height), 
                  levelsobject.level2_object(width,height), 
@@ -72,14 +70,13 @@ class Player:
         self.player_charge = pygame.image.load('image/player/playercharge.png').convert_alpha()
         self.player_jump = pygame.image.load('image/player/playerjump.png').convert_alpha()
         self.player_fell = pygame.image.load('image/player/playerfell.png').convert_alpha()
-        self.player_state = [self.player_stand, self.player_walk_1, self.player_charge, self.player_jump, self.player_fell]
+        self.player_state = [self.player_stand, self.player_walk_1, self.player_jump, self.player_fell]
         self.player_index = 0
 
         self.player_surf = self.player_state[self.player_index]
         self.player_rect = self.player_surf.get_rect()
         self.player_gravity = 0
         self.player_direction = 1
-        self.jumpCharge = 0
         self.midAir = False
         self.midStrafe = False
         self.onAir = 0
@@ -91,14 +88,11 @@ class Player:
         if self.midStrafe:
             self.player_index = 1
             self.player_surf = self.player_state[self.player_index]
-        elif self.jumpCharge != 0:
+        elif self.midAir:
             self.player_index = 2
             self.player_surf = self.player_state[self.player_index]
-        elif self.midAir:
-            self.player_index = 3
-            self.player_surf = self.player_state[self.player_index]
         elif self.tired:
-            self.player_index = 4
+            self.player_index = 3
             self.player_surf = self.player_state[self.player_index]
         else:
             self.player_index = 0
@@ -140,33 +134,41 @@ class AgentJEnv(gym.Env):
     def render(self):
         screen.blit(P.player_surf,P.player_rect)
         pygame.display.update()
+        # pygame.display.flip()
     
 # ===================================================================== #
 
 def start_game(player):
-    global game_state, level
+    global level
+    bgm_sound.stop()
     bgm_sound.play(-1)
-    game_state = 1
     level = 1
-    player.midAir = False
-    player.midStrafe = False
+    player.player_gravity = 0
     player.player_rect = player.player_surf.get_rect(midbottom= (width/2,700))
 # =========================================================================== #
 
 # ====================== STEP FUNCTION GAME ========================== #
 def step(action):
     global P
-    if action == 0:
-        pass
-    elif action == 1:
-        pass
-    elif action == 2:
-        pass
-    elif action == 3:
-        pass
-    elif action == 4:
-        pass
+    if not P.midAir and not P.tired:
+        if action == 0:
+            pass  # No action for action == 0
+        elif action == 1:
+            P.player_direction = -1
+            P.midStrafe = True
+        elif action == 2:
+            P.player_direction = 1
+            P.midStrafe = True
+        elif action == 3:
+            P.midStrafe = False
+            P.player_gravity = -13
+            P.midAir = True
+        elif action == 4:
+            P.midStrafe = False
+            P.player_gravity = -19.5
+            P.midAir = True
 
+    game_logic()
     # Run the game 1 frame
 
     # Get the new state
@@ -179,9 +181,8 @@ def step(action):
     return state, reward, done, info
 # ==================================================================== #
 
-start_game(P)
-
-while True:
+def game_logic():
+    global level
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -192,129 +193,124 @@ while True:
             if P.player_rect.collidepoint(event.pos):
                 P.player_gravity = -15
         
-        if game_state == 1:
-            # Keyboard press down
-            if event.type == pygame.KEYDOWN:
-                # if event.key == pygame.K_SPACE and not P.midAir and not P.tired:
-                #     P.jumpCharge += 0.3
-                #     P.midStrafe = False
-                if event.key == pygame.K_a and not P.midAir and not P.tired:
-                    P.player_direction = -1
-                    if P.jumpCharge == 0:
-                        P.midStrafe = True
-                if event.key == pygame.K_d and not P.midAir and not P.tired:
-                    P.player_direction = 1
-                    if P.jumpCharge == 0:
-                        P.midStrafe = True
-                if event.key == pygame.K_c and not P.midAir and not P.tired:
-                    P.midStrafe = False
-                    P.player_gravity = -13
-                    P.midAir = True
-                if event.key == pygame.K_v and not P.midAir and not P.tired:
-                    P.midStrafe = False
-                    P.player_gravity = -19.5
-                    P.midAir = True
-                # ============================================ #
-                if event.key == pygame.K_n:
-                    level += 1
-                    P.player_rect.y = 800
-                if event.key == pygame.K_m:
-                    level -= 1
-                    P.player_rect.y = 800
+        # Keyboard press down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a and not P.midAir and not P.tired:
+                P.player_direction = -1
+                P.midStrafe = True
+            if event.key == pygame.K_d and not P.midAir and not P.tired:
+                P.player_direction = 1
+                P.midStrafe = True
+            if event.key == pygame.K_c and not P.midAir and not P.tired:
+                P.midStrafe = False
+                P.player_gravity = -13
+                P.midAir = True
+                play_sound('jump')
+            if event.key == pygame.K_v and not P.midAir and not P.tired:
+                P.midStrafe = False
+                P.player_gravity = -19.5
+                P.midAir = True
+                play_sound('jump')
+            if event.key == pygame.K_r:
+                start_game(P)
+            # ============================================ #
+            if event.key == pygame.K_n:
+                level += 1
+                P.player_rect.y = 800
+            if event.key == pygame.K_m:
+                level -= 1
+                P.player_rect.y = 800
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a and P.midStrafe:
-                    P.midStrafe = False
-                if event.key == pygame.K_d and P.midStrafe:
-                    P.midStrafe = False
-                # if event.key == pygame.K_SPACE and not P.midAir and P.jumpCharge != 0:
-                #     P.player_gravity = -1.3 * P.jumpCharge
-                #     P.jumpCharge = 0
-                #     P.midAir = True
-                #     play_sound('jump')          
-
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a and P.midStrafe:
+                P.midStrafe = False
+            if event.key == pygame.K_d and P.midStrafe:
+                P.midStrafe = False        
 
     # Gameplay
-    if game_state == 1:
-        # Blit
-        for surf, rect, _ in levels_object[level-1]:
-            screen.blit(surf, rect)
+    # Blit
+    for surf, rect, _ in levels_object[level-1]:
+        screen.blit(surf, rect)
 
-        # Jumping 
-        if P.jumpCharge !=0 and P.jumpCharge <= 15:
-            P.jumpCharge += 0.3
-        P.player_gravity += 0.6
-        P.player_rect.y += P.player_gravity
-        if P.midAir:
-            P.player_rect.x += P.player_direction*10
-        # Move the Character
-        if P.midStrafe:
-            P.player_rect.x += P.player_direction*5
-        # Stop when touching a Border
-        if P.player_rect.right >= width:
-            P.midStrafe = False
-            P.player_rect.right = width
-            if P.midAir: 
-                P.player_direction *= -1
-                play_sound('wall')
-        if P.player_rect.left <= 0:
-            P.player_rect.left = 0
-            P.midStrafe = False
-            if P.midAir: 
-                P.player_direction *= -1
-                play_sound('wall')
+    # Jumping 
+    P.player_gravity += 0.6
+    P.player_rect.y += P.player_gravity
+    if P.midAir:
+        P.player_rect.x += P.player_direction*10
+    # Move the Character
+    if P.midStrafe:
+        P.player_rect.x += P.player_direction*5
+    # Stop when touching a Border
+    if P.player_rect.right >= width:
+        P.midStrafe = False
+        P.player_rect.right = width
+        if P.midAir: 
+            P.player_direction *= -1
+            play_sound('wall')
+    if P.player_rect.left <= 0:
+        P.player_rect.left = 0
+        P.midStrafe = False
+        if P.midAir: 
+            P.player_direction *= -1
+            play_sound('wall')
 
-        P.onAir += 1
-        for platform_surf, platform_rect, platform in levels_object[level-1]:
-            if platform:
-                if P.player_rect.colliderect(platform_rect):
-                    side = get_collision_side(P.player_rect, platform_rect)
-                    if side == 'top':
-                        P.player_rect.top = platform_rect.bottom
-                        if P.midAir: 
-                            P.player_gravity = 0
-                            play_sound('wall')
-                    elif side == 'bottom':
-                        if P.player_gravity >= 30:
-                            P.tired = True
-                            P.midStrafe = False
-                            play_sound('fell')
-                        if P.tired:
-                            P.rest += 1
-                            if P.rest >= 90:
-                                P.tired = False
-                                P.rest = 0
+    P.onAir += 1
+    for platform_surf, platform_rect, platform in levels_object[level-1]:
+        if platform:
+            if P.player_rect.colliderect(platform_rect):
+                side = get_collision_side(P.player_rect, platform_rect)
+                if side == 'top':
+                    P.player_rect.top = platform_rect.bottom
+                    if P.midAir: 
                         P.player_gravity = 0
-                        P.player_rect.bottom = platform_rect.top + 2
-                        P.midAir = False
-                    elif side == 'right':
-                        if not P.midStrafe: 
-                            P.player_direction *= -1
-                            play_sound('wall')
-                        P.player_rect.right = platform_rect.left
-                    elif side == 'left':
-                        if not P.midStrafe: 
-                            P.player_direction *= -1
-                            play_sound('wall')
-                        P.player_rect.left = platform_rect.right
+                        play_sound('wall')
+                elif side == 'bottom':
+                    if P.player_gravity >= 30:
+                        P.tired = True
+                        P.midStrafe = False
+                        play_sound('fell')
+                    if P.tired:
+                        P.rest += 1
+                        if P.rest >= 90:
+                            P.tired = False
+                            P.rest = 0
+                    P.player_gravity = 0
+                    P.player_rect.bottom = platform_rect.top + 2
+                    P.midAir = False
+                elif side == 'right':
+                    if not P.midStrafe: 
+                        P.player_direction *= -1
+                        play_sound('wall')
+                    P.player_rect.right = platform_rect.left
+                elif side == 'left':
+                    if not P.midStrafe: 
+                        P.player_direction *= -1
+                        play_sound('wall')
+                    P.player_rect.left = platform_rect.right
 
-        # check if the Player reach top or bottom
-        if P.player_rect.top <= 0:
-            P.player_rect.top += height
-            level += 1
-        elif P.player_rect.top >= height:
-            P.player_rect.top -= height
-            level -= 1
+    # check if the Player reach top or bottom
+    if P.player_rect.top <= 0:
+        P.player_rect.top += height
+        level += 1
+    elif P.player_rect.top >= height:
+        P.player_rect.top -= height
+        level -= 1
 
-        # Close the game if done
-        if level == len(levels_object):
-            if (P.player_rect.right >= 460 and P.player_rect.right <= 740) or (P.player_rect.left >= 460 and P.player_rect.left <= 740):
-                if P.player_rect.centery <= 630:
-                    pygame.quit()
-                    exit()
+    # Close the game if done
+    if level == len(levels_object):
+        if (P.player_rect.right >= 460 and P.player_rect.right <= 740) or (P.player_rect.left >= 460 and P.player_rect.left <= 740):
+            if P.player_rect.centery <= 630:
+                pygame.quit()
+                exit()
 
-        P.player_animation()
-        screen.blit(P.player_surf,P.player_rect)
+    P.player_animation()
+    screen.blit(P.player_surf,P.player_rect)
 
     pygame.display.update()
     clock.tick(fps)
+
+
+start_game(P)
+
+while True:
+    game_logic()
