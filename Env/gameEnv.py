d71044908ce7ff1c23415edc2b981d38f034c75a
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-11-10 12:44:27 +0100

    change the filename

--- Env/gameEnv.py
+++ Env/gameEnv.py
@@ -261,54 +261,58 @@ class AgentJEnv(gym.Env):
 
         self.P.player_animation()
 
-env = AgentJEnv(render_mode="human")
-state = env.reset()
-action = 0
-i = 0
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            env.close()
-            exit()
+
+
+
+#========================== MAIN FUNCTION ================================#
+# env = AgentJEnv(render_mode="human")
+# state = env.reset()
+# action = 0
+# i = 0
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             env.close()
+#             exit()
         
-        # click to get the Cord of the Cursor
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
+#         # click to get the Cord of the Cursor
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             print(event.pos)
         
-        # Keyboard press down
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                action = 1
-            if event.key == pygame.K_d:
-                action = 2
-            if event.key == pygame.K_c:
-                action = 3
-            if event.key == pygame.K_v:
-                action = 4
-            if event.key == pygame.K_r:
-                state = env.reset()
-            # ============================================ #
-            if event.key == pygame.K_n:
-                env.level += 1
-            if event.key == pygame.K_m:
-                env.level -= 1
+#         # Keyboard press down
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_a:
+#                 action = 1
+#             if event.key == pygame.K_d:
+#                 action = 2
+#             if event.key == pygame.K_c:
+#                 action = 3
+#             if event.key == pygame.K_v:
+#                 action = 4
+#             if event.key == pygame.K_r:
+#                 state = env.reset()
+#             # ============================================ #
+#             if event.key == pygame.K_n:
+#                 env.level += 1
+#             if event.key == pygame.K_m:
+#                 env.level -= 1
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a and env.P.midStrafe:
-                action = 0
-            if event.key == pygame.K_d and env.P.midStrafe:
-                action = 0  
+#         if event.type == pygame.KEYUP:
+#             if event.key == pygame.K_a and env.P.midStrafe:
+#                 action = 0
+#             if event.key == pygame.K_d and env.P.midStrafe:
+#                 action = 0  
 
-    # action = random.choice([0, 1, 2, 3, 4])
-    state, reward, done = env.step(action)
+#     # action = random.choice([0, 1, 2, 3, 4])
+#     state, reward, done = env.step(action)
 
-    # stop the jumping action
-    if action in (3,4):
-        action = 0
+#     # stop the jumping action
+#     if action in (3,4):
+#         action = 0
 
-    env.render()
+#     env.render()
 
-    if done:
-        break
+#     if done:
+#         break
 
-env.close()
+# env.close()
